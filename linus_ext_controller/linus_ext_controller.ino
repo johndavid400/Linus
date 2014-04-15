bb5b95d7227145495ccfff55d674e3c7c97bacf6
J.D. Warren <johndavid400@gmail.com>
2014-04-15 13:51:32 -0500

    added new simplified code for Linus

---
+++ linus_ext_controller/linus_ext_controller.ino
@@ -0,0 +1,116 @@
+// Linus the Line-bot
+// Follows a Black line on a White surface (poster-board and electrical tape).
+// Code by JDW 2014 – feel free to modify.
+
+int left_motor_dir(8);
+int left_motor_pwm(9);
+int right_motor_pwm(10);
+int right_motor_dir(11);
+
+// Create variables for sensor readings
+int left = 0;
+int left_center = 0;
+int center = 0;
+int right_center= 0;
+int right = 0;
+
+// value to define a threshold for whether reading white or black
+int threshold = 512;
+
+int speed_value = 255;
+int speed_pot;
+
+boolean debug = false;
+boolean use_speed_pot = false;
+
+void setup(){
+  Serial.begin(9600); // start serial monitor to see sensor readings
+  // declare left motor
+  pinMode(left_motor_pwm, OUTPUT);
+  pinMode(left_motor_dir, OUTPUT);
+  // declare right motor
+  pinMode(right_motor_pwm, OUTPUT);
+  pinMode(right_motor_dir, OUTPUT);
+  // set motors in position to go forward only
+  digitalWrite(left_motor_dir, HIGH);
+  digitalWrite(right_motor_dir, HIGH);
+}
+
+void update_sensors(){
+  left = analogRead(0);
+  left_center = analogRead(1);
+  center = analogRead(2);
+  right_center = analogRead(3);
+  right = analogRead(4);
+  
+  if (use_speed_pot){
+    // check value for speed potentiometer
+    speed_value = analogRead(5) / 4;
+  }
+}
+
+void loop(){
+
+  update_sensors();
+
+  // first, check the value of the center sensor
+  if (center < threshold){
+    // if center sensor value is below threshold, check surrounding sensors
+    if (left_center > threshold && right_center > threshold){
+      // if center is the only one reading black tape, drive forward
+      left_motor_forward();
+      right_motor_forward();
+    }
+    // you want the bot to stop when it reaches the black box.
+    else if (left < threshold && left_center < threshold && center < threshold && right_center < threshold && right < threshold){
+      //  if all sensors are reading black, stop Linus.
+      left_motor_stop();
+      right_motor_stop();
+    }
+  }
+  else {
+    // first check the left sensors
+    if (left < threshold || left_center < threshold){
+      left_motor_stop();
+      right_motor_forward();
+    }
+    // then check the right sensors
+    else if (right < threshold || right_center < threshold){
+      left_motor_forward();
+      right_motor_stop();
+    }
+  }
+  if (debug){
+    serial_print_stuff();
+  }
+}
+
+void serial_print_stuff(){
+  Serial.print(left);
+  Serial.print("   ");
+  Serial.print(left_center);
+  Serial.print("   ");
+  Serial.print(center);
+  Serial.print("   ");
+  Serial.print(right_center);
+  Serial.print("   ");
+  Serial.println(right);
+}
+
+void left_motor_forward(){
+  analogWrite(left_motor_pwm, speed_value);
+}
+
+void left_motor_stop(){
+  digitalWrite(left_motor_pwm, LOW);
+}
+
+void right_motor_forward(){
+  analogWrite(right_motor_pwm, speed_value);
+}
+
+void right_motor_stop(){
+  digitalWrite(right_motor_pwm, LOW);
+}
+
+
