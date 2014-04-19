f9e051bc8f326d8b28a394d956b39604d227e61c
JD <johndavid400@gmail.com>
2014-04-18 21:13:50 -0500

    updated comments and threshold value

--- linus_ext_controller/linus_ext_controller.ino
+++ linus_ext_controller/linus_ext_controller.ino
@@ -2,27 +2,31 @@
 // Follows a Black line on a White surface (poster-board and electrical tape).
 // Code by JDW 2014 – feel free to modify.
 
+// motor pins
 int left_motor_dir = 12;
 int left_motor_pwm = 3;
 int right_motor_pwm = 11;
 int right_motor_dir = 13;
 
-// Create variables for sensor readings
+// create variables for sensor readings
 int left = 0;
 int left_center = 0;
 int center = 0;
 int right_center= 0;
 int right = 0;
 
+// analog pin to connect potentiometer used for speed control
 int speed_pin = 0;
-int left_pin = 5;
-int left_center_pin = 4;
+
+// analog input pins for IR sensors
+int left_pin = 1;
+int left_center_pin = 2;
 int center_pin = 3;
-int right_center_pin = 2;
-int right_pin = 1;
+int right_center_pin = 4;
+int right_pin = 5;
 
 // value to define a threshold for whether reading white or black
-int threshold = 700;
+int threshold = 900;
 
 int speed_value = 64;
 int speed_pot;
@@ -66,6 +70,7 @@ void check_speed_control(){
 }
 
 void update_sensors(){
+  // read values for each IR sensor
   left = analogRead(left_pin);
   left_center = analogRead(left_center_pin);
   center = analogRead(center_pin);
@@ -84,13 +89,12 @@ void loop(){
 
   // first, check the value of the center sensor
   if (center < threshold){
-    // if center sensor value is below threshold, check surrounding sensors
+    // if center sensor value is below threshold (on the black line), check surrounding sensors
     if (left_center > threshold && right_center > threshold){
-      // if center is the only one reading black tape, drive forward
-      left_motor_forward();
-      right_motor_forward();
+      // if center is the only one reading the black line, drive forward
+      left_motor_forward(speed_value);
+      right_motor_forward(speed_value);
     }
-    // you want the bot to stop when it reaches the black box.
     else if (left < threshold && left_center < threshold && center < threshold && right_center < threshold && right < threshold){
       //  if all sensors are reading black, stop Linus.
       left_motor_stop();
@@ -98,23 +102,21 @@ void loop(){
     }
   }
   else {
-    // first check the left sensors
     if (left < threshold && right > threshold){
       left_motor_stop();
-      right_motor_forward();
+      right_motor_forward(speed_value);
     }
-    // then check the right sensors
-    else if (left < threshold && right > threshold){
-      left_motor_forward();
+    else if (left > threshold && right < threshold){
+      left_motor_forward(speed_value);
       right_motor_stop();
     }
     else if (left_center < threshold && right_center > threshold){
-      left_motor_stop();
-      right_motor_forward();
+      left_motor_forward(speed_value / 2);
+      right_motor_forward(speed_value);
     }
     else if (left_center > threshold && right_center < threshold){
-      left_motor_forward();
-      right_motor_stop();
+      left_motor_forward(speed_value);
+      right_motor_forward(speed_value / 2);
     }
   }
   if (debug){
@@ -134,28 +136,28 @@ void serial_print_stuff(){
   Serial.println(right);
 }
 
-void left_motor_forward(){
+void left_motor_forward(int spd){
   digitalWrite(left_motor_dir, LOW);
-  analogWrite(left_motor_pwm, speed_value);
+  analogWrite(left_motor_pwm, spd);
 }
 
-void left_motor_reverse(){
+void left_motor_reverse(int spd){
   digitalWrite(left_motor_dir, HIGH);
-  analogWrite(left_motor_pwm, speed_value);
+  analogWrite(left_motor_pwm, spd);
 }
 
 void left_motor_stop(){
   digitalWrite(left_motor_pwm, LOW);
 }
 
-void right_motor_forward(){
+void right_motor_forward(int spd){
   digitalWrite(right_motor_dir, LOW);
-  analogWrite(right_motor_pwm, speed_value);
+  analogWrite(right_motor_pwm, spd);
 }
 
-void right_motor_reverse(){
+void right_motor_reverse(int spd){
   digitalWrite(right_motor_dir, HIGH);
-  analogWrite(right_motor_pwm, speed_value);
+  analogWrite(right_motor_pwm, spd);
 }
 
 void right_motor_stop(){
